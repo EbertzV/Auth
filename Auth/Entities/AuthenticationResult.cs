ffe31a5c8f38b6ff42ce1fa7f5df3a392b2f3d27
EbertzV <ebertzv@outlook.com>
2023-11-09 01:06:15 -0300

    Adicionar arquivos de projeto.

---
+++ Auth/Entities/AuthenticationResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace Auth.Entities
+{
+    public sealed class AuthenticationResult
+    {
+        private AuthenticationResult(bool isAuthenticated, string token, HttpStatusCode statusCode)
+        {
+            IsAuthenticated = isAuthenticated;
+            Token = token;
+            StatusCode = statusCode;
+        }
+
+        public static AuthenticationResult NewFailure(HttpStatusCode statusCode)
+            => new AuthenticationResult(false, string.Empty, statusCode);
+
+        public static AuthenticationResult NewSuccess(string token)
+            => new AuthenticationResult(true, token, HttpStatusCode.OK);
+
+        public bool IsAuthenticated { get; }
+        public string Token { get; }
+        public HttpStatusCode StatusCode { get; }
+    }
+}
---
+++ Auth/Services/AuthenticationService.cs
@@ -0,0 +1,42 @@
+﻿using Auth.Entities;
+using System.Net;
+using System.Text.Json;
+
+namespace Auth.Services
+{
+    public sealed class AuthenticationService
+    {
+        private readonly string _endpointUrl;
+
+        public AuthenticationService(string endpointUrl)
+        {
+            _endpointUrl = endpointUrl;
+        }
+
+        public async Task<AuthenticationResult> TryAuthenticate(User user)
+        {
+            var client = new HttpClient();
+
+            var message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(_endpointUrl)
+            };
+
+            message.Headers.Clear();
+            message.Content = new StringContent(JsonSerializer.Serialize(user));
+
+            var result = await client.SendAsync(message);
+
+            if(result.StatusCode != HttpStatusCode.OK)
+                return AuthenticationResult.NewFailure(result.StatusCode);
+
+            using var stream = await result.Content.ReadAsStreamAsync();
+            using var reader = new StreamReader(stream);
+            var authResult = await reader.ReadToEndAsync();
+            if (string.IsNullOrWhiteSpace(authResult))
+                return AuthenticationResult.NewFailure(result.StatusCode);
+            return AuthenticationResult.NewSuccess(authResult);
+        }
+    }
+}
